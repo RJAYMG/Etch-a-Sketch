56a85b18138f14708f466d47e22764f228f5e974
RJAY <garciarayshanjarvis@users.noreply.github.com>
2026-02-13 20:49:21 -0800

    so right now im using specific sizes for the individual squares, which is now my problem if i want to create a new grid. so i'll commit here if i ever make a mistake.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,35 @@
 const container = document.querySelector("#container");
+const wrapper = document.querySelector("#wrapper")
+const square = document.createElement("div");
+const reset = document.createElement("button");
+const newGrid = document.createElement("button");
+
+reset.textContent = "Reset";
+reset.style.borderRadius = "4px";
+reset.style.color = "white";
+reset.style.backgroundColor = "blue";
+reset.style.padding = "0.3";
+
+wrapper.appendChild(reset);
+
+square.style.height = "1em";
+square.style.width = "1em";
+square.style.border = "1px solid black";
 
 for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.style.height = "1em";
-    square.style.width = "1em";
-    square.style.border = "1px solid black";
-    container.appendChild(square);
+    const clone = square.cloneNode(true);
+    container.appendChild(clone);
+    clone.addEventListener("mouseenter", () => {
+    clone.style.backgroundColor = "darkblue";
+    });
+    clone.addEventListener("mouseleave", () => {
+        clone.style.backgroundColor = "blue";
+    });
+    reset.addEventListener("click", () => {
+        clone.style.backgroundColor = "";
+    })
+};
+
+function createGrid(squareAmount) {
+    
 }
